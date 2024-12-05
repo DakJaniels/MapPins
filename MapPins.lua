2aaf3851fa69568851dc959fa0cfa6c9bc08fd4e
art1ink <art-klyop@yandex.ru>
2024-12-05 22:38:10 +0300

    1.98.5
    add Chinese localization by @ADGbehCFI

--- MapPins.lua
+++ MapPins.lua
@@ -316,6 +316,44 @@ local Localization={
 		Necrom = "Necrom",
 		Gold_Road = "Gold Road",
 		},
+	zh={--provided by @ADGbehCFI 1.98
+		--Water
+		Lake="湖泊",Foul="脏水",River="河流",Salt="咸水",Oily="油污",Mystic="神秘",Running="Running",
+		--Portals
+		Portals="传送门",Celestial_Rifts="天界裂缝",Dark_Fissures="黑暗裂缝",Oblivion_Portals="湮灭传送门",Shadow_Fissures="阴影裂缝",Lava_Lashers="岩浆驯服者",
+		--Filters
+		Delve_bosses="世界BOSS",Delve_bosses_done="世界BOSS(已完成)",
+		Skyshards="天空碎片",Skyshards_done="天空碎片 (已完成)",
+		Lore_books="沙利多藏书",Lore_books_done="沙利多藏书(已完成)",
+		Treasure_Maps="调查报告",
+		Treasure_Chests="藏宝图",
+		Unknown_POI="未知地点",
+		Undaunted="无畏者",
+		Time_Rifts="时间裂缝",
+		Shrines="祭坛",
+		Fishing_Nodes="钓鱼节点",
+		Volendrung="沃伦神锤",
+		Antiquities="古物",Antiquity_Leads="考古线索",
+		Imperial_City="帝都",IC_Bosses="游荡恐怖",IC_Respawns="重生点",
+		Cunning_Scamp="狡诈小鬼",Trove_Scamp="宝藏小鬼",
+		World_achievements="世界成就",
+		Orsinium_world_event="奥辛纽姆赞助者",
+		Summerset_world_event="透过现象看本质",
+		Random_Encounters="随机遭遇",
+		--DLC
+		Orsinium="奥辛纽姆",
+		Thieves_guild="盗贼公会",
+		Morrowind="晨风",
+		Summerset="夏暮岛",
+		Clockwork_City="发条城",
+		Murkmire="幽暗沼泽",
+		Elsweyr="艾斯维尔",
+		Blackwood="黑木",
+		Greymoor="灰沼",
+		High_Isle="高岛",
+		Necrom="死灵之地",
+		Gold_Road="黄金大道",
+		},
 	}
 local lang=GetCVar("language.2") if not Localization[lang] then lang="en" end
 local function Loc(string)
